7280d841021dc6368f1abbfb9e6de04e4e1fc479
tbwester <thomas_w@live.com>
2024-07-24 11:40:29 -0500

    make it a package & add an example

--- daqrunlogger/__init__.py
+++ daqrunlogger/__init__.py
@@ -1,15 +1,15 @@
-from daqrunlogger import DAQRunLogger, StdoutDAQRunLogger
-from daqloggerworker import DAQLoggerWorker
+from .daqrunlogger import DAQRunLogger, StdoutDAQRunLogger
+from .daqloggerworker import DAQLoggerWorker
 
 try:
     from googleapiclient.discovery import build
     from google.oauth2 import service_account
-    from GoogleSheetsDAQRunLogger import GoogleSheetsDAQRunLogger
+    from .googlesheetsdaqrunlogger import GoogleSheetsDAQRunLogger
 except ImportError:
     pass
 
 try:
     from sbndprmdaq.eclapi import ECL
-    from ecldaqrunlogger import ECLDAQRunLogger
+    from .ecldaqrunlogger import ECLDAQRunLogger
 except ImportError:
     pass
