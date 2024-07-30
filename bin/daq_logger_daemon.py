c1a1fd64b80db800762692b3ff2802e44d2c0533
tbwester <thomas_w@live.com>
2024-07-30 09:15:58 -0500

    make it a package & add an example

--- bin/daq_logger_daemon.py
+++ bin/daq_logger_daemon.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
 import time
 from queue import Queue
+import datetime
 
 import daemon
 
-from daqrunlogger import GoogleSheetsDAQRunLogger, LoggerWorker
+from daqrunlogger import StdoutDAQRunLogger, DAQLoggerWorker
 
 
 def main():
@@ -18,15 +20,18 @@ def main():
     workers = []
     for logger in loggers:
         q = Queue()
-        w = LoggerWorker(logger, q)
+        w = DAQLoggerWorker(logger, q)
         queues.append(q)
         workers.append(w)
 
-        worker.start()
+        w.start()
 
+    print(f'Started with {len(workers)} workers.')
+    new_run = False
     while True:
         # check the DAQ log for new runs
         if not new_run:
+            print('No new run, sleeping...')
             time.sleep(1)
             continue
 
@@ -36,5 +41,5 @@ def main():
 
 
 if __name__ == '__main__':
-    with daemon.DaemonContext():
+    with daemon.DaemonContext(stdout=sys.stdout, stderr=sys.stderr, detach_process=False):
         main()
