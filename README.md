72b3643cae2a57b377052977005a9873edadd246
tbwester <thomas_w@live.com>
2024-07-30 09:15:58 -0500

    initial commit of daemon

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Collection of short classes to log DAQ information to different platforms.
 
-Targeted platforms:
+Planned platforms:
 - Google sheets
 - Fermilab ECL (electronic logbook). Requires the ![sbndprmdaq](https://github.com/marcodeltutto/SBNDPurityMonitorDAQ) package.
+- Post JSON string of the current run to a server for other APIs
+
+Additionally, we provide a daemon class to handle executing the loggers on
+different threads.
---
+++ daqrunlogger/DAQLoggerDaemon.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import time
+from queue import Queue
+from threading import Thread
+from datetime import datetime
+
+from DAQRunLogger import RunInfo, DAQRunLogger
+
+
+
+class LoggerWorker(Thread):
+    """Class to be assigned to a thread. Listens for RunInfo objects and calls
+    its DAQRunLogger's log_run method when available."""
+    def __init__(self, logger: DAQRunLogger, queue: Queue, timeout: int=1):
+        super().__init__()
+        self._logger = logger
+        self._queue = queue
+        # in seconds
+        self._timeout = timeout
+        self._stopped = False
+
+    
+    def stop(self):
+        self._stopped = True
+
+    def run(self):
+        while not self._stopped:
+            if self._queue.empty():
+                time.sleep(self._timeout)
+                continue
+
+            info = self._queue.get()
+            self._logger.log_run(info)
+            self._queue.task_done()
+
+
+if __name__ == '__main__':
+    from DAQRunLogger import StdoutDAQRunLogger
+    info_queue = Queue()
+    logger = StdoutDAQRunLogger()
+    worker = LoggerWorker(logger, info_queue)
+    worker.start()
+
+    info = RunInfo(17215, datetime.now(), 'bnbTest', end_time=datetime.now(), comments='test comment')
+    for i in range(5):
+        info_queue.put(info)
+        time.sleep(1)
+
+    while not info_queue.empty():
+        sleep(1)
+    
+    worker.stop()
